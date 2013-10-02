032aaecf0467986163e60fdf8a18320be9a1351d
Hack Reactor Students <github@hackreactor.com>
2013-10-02 12:36:08 -0700

    added watch tasks concurrent and nodemon to both package and gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,10 +13,45 @@ module.exports = function(grunt){
       files: {
         'public/css/styles.css': 'public/css/styles.styl'
       }
+    },
+    nodemon: {
+      dev: {
+        options:{
+          file: 'app/server.js',
+          args: [],
+          ignoredFiles: ['README.md', 'node_modules/**', '.DS_Store'],
+          watchedFolders: ['app', 'config'],
+          debug: true,
+          delayTime: 1,
+          env: {
+            PORT: 3000
+          },
+          cwd: __dirname
+        }
+      }
+    },
+    watch: {
+      browserify:{
+        files: ['public/js/**/*.js'],
+        tasks: ['browserify']
+      },
+      stylus: {
+        files: ['public/css/styles.styl'],
+        tasks: ['stylus']
+      }
+    },
+    concurrent: {
+      tasks: ['nodemon', 'watch'],
+      options: {
+        logConcurrentOutput: true
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-stylus');
-
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.registerTask('default', 'concurrent');
 };
