6e1dabbb895a132dd3dfc08f7dc7ce430687373d
Blake West <bwest87@gmail.com>
2013-10-03 10:58:24 -0700

    added function for posting new link data.

--- public/js/controllers/form.js
+++ public/js/controllers/form.js
@@ -1,5 +1,7 @@
-angular.module('readUp.controllers', []).controller('FormController', ['$scope', function($scope) {
-  $scope.print = function(post){
-    console.log(post);
+angular.module('readUp.controllers', []).controller('FormController', ['$scope', '$http', function($scope, $http) {
+  $scope.send = function(item){
+    $http.post('/items', item).success(function() {
+      console.log('we added the link bitches!!!!');
+    });
   };
 }]);
