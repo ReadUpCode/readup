e3ed2a6c94529143b5a53213b60a66c706dba60d
Hack Reactor Students <github@hackreactor.com>
2013-10-03 15:41:46 -0700

    created function to display input in modal

--- public/js/controllers/form.js
+++ public/js/controllers/form.js
@@ -1,5 +1,7 @@
 angular.module('readUp.controllers', []).controller('FormController', ['$scope', '$http', '$modal', function($scope, $http, $modal) {
-  $scope.modal = {content: $scope.item}
+  $scope.updateModal = function(item){
+    $scope.modal = {content: item.link};
+  };
   $scope.send = function(item){
     $http.post('/items', item).success(function() {
       console.log('we added the link bitches!!!!');
