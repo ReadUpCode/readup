8eb0f95bc9fbb112096830ed9aa8fb2e41c99d2f
Hack Reactor Students <github@hackreactor.com>
2013-10-03 12:35:14 -0700

    using node-orm2, but feel the documentation is sparce, and are investigating Sequelize

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 var express = require('express');
-var path = require('path');
 var app = express();
+var orm = require('orm');
+var modts = require('orm-timestamps'); //plugin for node-orm2 for timestamps
+
+orm.connect('mysql://root@localhost/readup', function(err, db){
+  if(err){
+    console.log(err);
+  } else {
+    console.log('GOOD');
+  }
+
+  db.use(modts, {
+        createdProperty: 'created_at',
+        modifiedProperty: 'modified_at',
+        dbtype: { type: 'date', time: true },
+        now: function() { return new Date(); }
+  });
+
+
+  var User = db.define("user", {
+        name: String,
+        email: Number,
+        karma: Number
+  }, {
+    timestamp: true
+  });
+});
+
 
 app.configure(function(){
   app.use(express.static(__dirname + "/public"));
