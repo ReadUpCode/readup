53536266d915e3d5247eb7f3ddec5b312cb6197b
Blake West <bwest87@gmail.com>
2013-10-03 12:43:02 -0700

    added http module, and started function for modal

--- public/js/controllers/form.js
+++ public/js/controllers/form.js
@@ -1,4 +1,5 @@
-angular.module('readUp.controllers', []).controller('FormController', ['$scope', '$http', function($scope, $http) {
+angular.module('readUp.controllers', []).controller('FormController', ['$scope', '$http', '$modal', function($scope, $http, $modal) {
+  $scope.modal = {content: $scope.item}
   $scope.send = function(item){
     $http.post('/items', item).success(function() {
       console.log('we added the link bitches!!!!');
