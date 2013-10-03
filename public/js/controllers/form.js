2b09592f8a507f1f995b4a335282ecf3b5c59eff
Hack Reactor Students <github@hackreactor.com>
2013-10-03 16:42:08 -0700

    refactored send function

--- public/js/controllers/form.js
+++ public/js/controllers/form.js
@@ -1,11 +1,11 @@
 angular.module('readUp.controllers', []).controller('FormController', ['$scope', '$http', '$modal', function($scope, $http, $modal) {
-  $scope.tags = [];
-  $scope.send = function(item){
-    $http.post('/items', item).success(function() {
+  $scope.item = {tags : {}};
+  $scope.send = function(){
+    $http.post('/items', $scope.item).success(function() {
       console.log('we added the link bitches!!!!');
     });
   };
   $scope.addTag = function(tag){
-    $scope.tags.push(tag);
+    $scope.item.tags[tag] = tag;
   };
 }]);
