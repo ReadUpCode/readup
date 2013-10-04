0ec99e32d24d7b7defd20fe8fd9ecffe281e4bf5
Hack Reactor Students <github@hackreactor.com>
2013-10-04 15:38:17 -0700

    changed to reflect real data from server

--- public/js/factories/tagsFactory.js
+++ public/js/factories/tagsFactory.js
@@ -5,16 +5,16 @@ factories.factory('tagsFactory', function($http) {
   factory.getAllTags = function(){
     $http.get('/tags').success(function(res){
       factory.popularTags = res;
-    })
-  }
+    });
+  };
   //factory.popularTags =  ['js', 'backbone.js', 'python', 'c', 'package managers', 'yourmom.js', 'batman.js', 'fangular'];
   factory.getTagInfo = function(tag) {
     // we're most likely going to need to also pass in the item id to this function
-    var requestURL = '/tags/' + tag + '/items';
+    var requestURL = '/tags/' + tag.id + '/items';
+    factory.curTag = tag.name;
     $http.get(requestURL).success(function(res){
       factory.curLinks = res;
     });
-    factory.curTag = tag;
     // factory.curLinks = [{score: 45, url: 'www.awesome.com/' + factory.curTag},
     //                     {score: 3, url: 'www.greattechblog.com/' + factory.curTag},
     //                     {score: 6, url: 'www.thisissweet.com/' + factory.curTag}];
