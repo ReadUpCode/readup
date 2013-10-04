b8c5ebd645d4f8c9e4cee55934fcb40dc46850eb
Hack Reactor Students <github@hackreactor.com>
2013-10-03 22:41:37 -0700

    changed home view and added tag view to handle individual pages for tags

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -1,11 +1,20 @@
 ;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-angular.module('readUp', ['ngRoute','readUp.factories', 'readUp.controllers','$strap.directives']);
+var app = angular.module('readUp', ['readUp.factories', 'readUp.controllers','$strap.directives'], function($routeProvider) {
+  $routeProvider.when('/', {
+      controller: 'HomeController',
+      templateUrl: '../partials/home_view.html'
+    });
+  $routeProvider.when('/:tag', {
+      controller: 'HomeController',
+      templateUrl: '../partials/tag_view.html'
+    });
+});
 var factories = angular.module('readUp.factories', []);
 var controllers = angular.module('readUp.controllers', ['readUp.factories']);
-var directives = angular.module('readUp.directives', []);
+// var directives = angular.module('readUp.directives', []);
 
 module.exports.controllers = controllers;
-module.exports.directives = directives;
+// module.exports.directives = directives;
 module.exports.factories = factories;
 
 },{}],2:[function(require,module,exports){
@@ -25,8 +34,16 @@ controllers.controller('FormController', ['$scope', '$http', '$modal', function(
 },{"../app.js":1}],3:[function(require,module,exports){
 var controllers = require('../app.js').controllers;
 
-controllers.controller('HomeController', ['$scope', '$http', 'tagsFactory', function($scope, $http, tagsFactory){
+controllers.controller('HomeController', ['$scope', '$http', '$location', 'tagsFactory', function($scope, $http, $location, tagsFactory){
   $scope.tags = tagsFactory.tags;
+  $scope.cur = {};
+  $scope.changeView = function(tag) {
+    $scope.cur.tag = tag;
+    console.log($scope.cur.tag);
+    $scope.cur.tagLinks = ['www.awesomeblog.com/' + tag, 'www.everydocs.com/' + tag];
+    console.log($scope.cur.tagLinks);
+    $location.path('/' + tag);
+  };
 }]);
 },{"../app.js":1}],4:[function(require,module,exports){
 var factories = require('../app.js').factories;
