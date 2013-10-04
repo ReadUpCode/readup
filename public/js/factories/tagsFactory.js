ad1b8be08f83cf5bd91186b055626dc7a9fd7ce5
Hack Reactor Students <github@hackreactor.com>
2013-10-04 12:10:43 -0700

    added score property to our fake data

--- public/js/factories/tagsFactory.js
+++ public/js/factories/tagsFactory.js
@@ -5,7 +5,9 @@ factories.factory('tagsFactory', function() {
   factory.popularTags =  ['js', 'backbone.js', 'python', 'c', 'package managers', 'yourmom.js', 'batman.js', 'fangular'];
   factory.getTagInfo = function(tag) {
     factory.curTag = tag;
-    factory.curLinks = ['www.awesome.com/' + factory.curTag, 'www.greattechblog.com/' + factory.curTag];
+    factory.curLinks = [{score: 45, url: 'www.awesome.com/' + factory.curTag},
+                        {score: 3, url: 'www.greattechblog.com/' + factory.curTag},
+                        {score: 6, url: 'www.thisissweet.com/' + factory.curTag}];
   };
 
   return factory;
