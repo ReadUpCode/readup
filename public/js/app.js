e3a6624bc37231574a33326d504a907ae3425aaf
Hack Reactor Students <github@hackreactor.com>
2013-10-03 22:40:39 -0700

    added routes to the app to handle arbitrary tag pages

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,17 @@
-angular.module('readUp', ['ngRoute','readUp.factories', 'readUp.controllers','$strap.directives']);
+var app = angular.module('readUp', ['readUp.factories', 'readUp.controllers','$strap.directives'], function($routeProvider) {
+  $routeProvider.when('/', {
+      controller: 'HomeController',
+      templateUrl: '../partials/home_view.html'
+    });
+  $routeProvider.when('/:tag', {
+      controller: 'HomeController',
+      templateUrl: '../partials/tag_view.html'
+    });
+});
 var factories = angular.module('readUp.factories', []);
 var controllers = angular.module('readUp.controllers', ['readUp.factories']);
-var directives = angular.module('readUp.directives', []);
+// var directives = angular.module('readUp.directives', []);
 
 module.exports.controllers = controllers;
-module.exports.directives = directives;
+// module.exports.directives = directives;
 module.exports.factories = factories;
