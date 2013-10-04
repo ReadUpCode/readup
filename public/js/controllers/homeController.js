398e4328931abd3125e1728d3d4f360410e236e5
Hack Reactor Students <github@hackreactor.com>
2013-10-03 22:39:44 -0700

    added change view function. It changes view, but can't seem to get access to new scope variables. wip

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,5 +1,13 @@
 var controllers = require('../app.js').controllers;
 
-controllers.controller('HomeController', ['$scope', '$http', 'tagsFactory', function($scope, $http, tagsFactory){
+controllers.controller('HomeController', ['$scope', '$http', '$location', 'tagsFactory', function($scope, $http, $location, tagsFactory){
   $scope.tags = tagsFactory.tags;
+  $scope.cur = {};
+  $scope.changeView = function(tag) {
+    $scope.cur.tag = tag;
+    console.log($scope.cur.tag);
+    $scope.cur.tagLinks = ['www.awesomeblog.com/' + tag, 'www.everydocs.com/' + tag];
+    console.log($scope.cur.tagLinks);
+    $location.path('/' + tag);
+  };
 }]);
