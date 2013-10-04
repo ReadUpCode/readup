35da4eae68d5d4dfc15e1d6dad912cd23a57f558
Hack Reactor Students <github@hackreactor.com>
2013-10-04 11:09:33 -0700

    changed initial tags to popular tags, and added in a call to the service function

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,9 +1,9 @@
 var controllers = require('../app.js').controllers;
 
 controllers.controller('HomeController', ['$scope', '$http', '$location', 'tagsFactory', function($scope, $http, $location, tagsFactory){
-  $scope.tags = tagsFactory.tags;
-  $scope.cur = {};
+  $scope.tags = tagsFactory.popularTags;
   $scope.changeView = function(tag) {
+    tagsFactory.getTagInfo(tag);
     $location.path('/' + tag);
   };
 }]);
