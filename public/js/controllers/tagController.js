da9b9f717b8a43a8cddd901d8d7a802d44d2af3c
Hack Reactor Students <github@hackreactor.com>
2013-10-04 11:10:36 -0700

    brought in use of factory to know current tag that's being viewed

--- public/js/controllers/tagController.js
+++ public/js/controllers/tagController.js
@@ -1,6 +1,6 @@
 var controllers = require('../app.js').controllers;
 
-controllers.controller('TagController', ['$scope', '$routeParams', function($scope, $routeParams) {
-  $scope.tag = $routeParams.tag;
-  $scope.links = ['www.awesomeblog.com/' + $scope.tag, 'www.docs.com/' + $scope.tag];
+controllers.controller('TagController', ['$scope', '$routeParams', 'tagsFactory', function($scope, $routeParams, tagsFactory) {
+  $scope.tag = tagsFactory.curTag;
+  $scope.links = tagsFactory.curLinks;
 }]);
