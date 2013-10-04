f6e36c3faf4cc02925d70300502968bee906fef8
Hack Reactor Students <github@hackreactor.com>
2013-10-03 20:31:42 -0700

    writing function to return all items for user using 'eager loading'

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -3,9 +3,17 @@ var express = require('express');
 var sequelize = new Sequelize('mysql://root@127.0.0.1/readup', {});
 var db = require('../../config/db');
 
-
+var User = db.User;
 var Item = db.Item;
 var Tag = db.Tag;
+
+
+exports.findAllItemsForUser = function(req, res){
+  Item.findAll({ include: [User], where: {UserId: req.UserId}}).success(function(items){
+    console.log(JSON.stringify(items));
+  });
+};
+
 
 exports.create = function(req, res){
   Item.create({ title: req.body.title, link: req.body.link }).success(function(item) {
