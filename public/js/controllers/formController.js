11bfe1d552a2355f8d4d50f5f3d16715fdc884bf
Hack Reactor Students <github@hackreactor.com>
2013-10-04 20:22:44 -0700

    added ability for multiple tags to be entered

--- public/js/controllers/formController.js
+++ public/js/controllers/formController.js
@@ -8,6 +8,13 @@ controllers.controller('FormController', ['$scope', '$http', '$modal', function(
     });
   };
   $scope.addTag = function(tag){
-    $scope.item.tags[tag] = tag;
+    var allTags = tag.split(',');
+    for (var i = 0; i < allTags.length; i++) {
+      var trimmed = allTags[i].trim();
+      $scope.item.tags[trimmed] = trimmed;
+    }
+  };
+  $scope.removeTag = function(tag){
+    delete $scope.item.tags[tag];
   };
 }]);
