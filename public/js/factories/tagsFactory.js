b3699e0be743193454aac5d53c62e611e83dbe36
macqueen <jessmacqueen@gmail.com>
2013-10-05 11:12:13 -0700

    swapped tag id for tag name in request

--- public/js/factories/tagsFactory.js
+++ public/js/factories/tagsFactory.js
@@ -10,11 +10,12 @@ factories.factory('tagsFactory', function($http) {
   //factory.popularTags =  ['js', 'backbone.js', 'python', 'c', 'package managers', 'yourmom.js', 'batman.js', 'fangular'];
   factory.getTagInfo = function(tag) {
     // we're most likely going to need to also pass in the item id to this function
-    var requestURL = '/tags/' + tag.id + '/items';
+    var requestURL = '/tags/' + tag.name + '/items';
     factory.curTag = tag.name;
     $http.get(requestURL).success(function(data){
       //CHANGE THIS TO TAKE OUT THE ZERO. IT"S BEEN CHANGED ON THE SERVER SIDE
-      factory.curLinks = data[0].items;
+      console.log(data);
+      factory.curLinks = data.items;
     });
     // factory.curLinks = [{score: 45, url: 'www.awesome.com/' + factory.curTag},
     //                     {score: 3, url: 'www.greattechblog.com/' + factory.curTag},
