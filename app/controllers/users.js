f7fad41885a4d1f4236ec42af44ec96651c6939f
Hack Reactor Students <github@hackreactor.com>
2013-10-05 19:09:07 -0700

    writing github user id to our DB

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,8 +5,6 @@ var sequelize = db.sequelize;
 var User = db.User;
 var Item = db.Item;
 
-var users = {};
-var userIdCount = 1;
 exports.getAllItemsForUser = function(req, res){
   User.findAll({include: [Item], where: {id: req.params.id } }).success(function(items){
     res.send(items);
@@ -14,8 +12,9 @@ exports.getAllItemsForUser = function(req, res){
 };
 
 exports.addGitHubUser = function(ghUserId){
-  users[userIdCount] = ghUserId;
-  userIdCount++;
-  console.log('users in our DB! ',users);
+  User.findOrCreate({github_id: ghUserId}, {name: 'blake'})
+    .success(function(){
+      console.log('users in our DB! ');
+    });
   return ghUserId;
 };
