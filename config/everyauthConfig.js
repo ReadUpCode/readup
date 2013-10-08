a1dd2ec5bbe68a858b5b9183c348c65fc898934c
macqueen <jessmacqueen@gmail.com>
2013-10-07 20:45:28 -0700

    requesting user email from github and writing all user info to DB

--- config/everyauthConfig.js
+++ config/everyauthConfig.js
@@ -4,23 +4,21 @@ var https = require('https');
 
 everyauth.debug = true;
 
+// everyauth.everymodule.findUserById(function(userId, cb) {
+//     console.log('findByUserId called');
+//     UserService.findOne({ id: userId }, function(err, user) {
+//         return cb(err, user);
+//     });
+// });
+
 everyauth.github
   .scope('user:email')
   .appId(process.env.APP_ID)
   .appSecret(process.env.APP_SECRET)
   .findOrCreateUser(function(session, accessToken, accessTokenExtra, ghUser) {
     // TODO: write find user function
-    getGHemail(accessToken);
-    return false || addGitHubUser(ghUser.id);
+    console.log('ghUser in findOrCreate everyauth: ', ghUser);
+    return false || addGitHubUser(ghUser, accessToken);
   })
   .entryPath('/auth/github')
   .redirectPath('/');
-
-
- var getGHemail = function(token){
- 	var requestURL = 'https://api.github.com/user/emails?access_token=' + token;
-	https.get(requestURL, function(res) {
-		res.on('data', function (chunk) {
-    });
-	})
- };
