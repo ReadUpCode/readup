51dba0d891d22f40d81fd3aedc4bb540aa4d81e9
macqueen <jessmacqueen@gmail.com>
2013-10-07 19:47:23 -0700

    getting github email

--- config/everyauthConfig.js
+++ config/everyauthConfig.js
@@ -1,14 +1,26 @@
 var everyauth = require('everyauth');
 var addGitHubUser = require('../app/controllers/users.js').addGitHubUser;
+var https = require('https');
 
 everyauth.debug = true;
 
 everyauth.github
+  .scope('user:email')
   .appId(process.env.APP_ID)
   .appSecret(process.env.APP_SECRET)
   .findOrCreateUser(function(session, accessToken, accessTokenExtra, ghUser) {
     // TODO: write find user function
+    getGHemail(accessToken);
     return false || addGitHubUser(ghUser.id);
   })
   .entryPath('/auth/github')
   .redirectPath('/');
+
+
+ var getGHemail = function(token){
+ 	var requestURL = 'https://api.github.com/user/emails?access_token=' + token;
+	https.get(requestURL, function(res) {
+		res.on('data', function (chunk) {
+    });
+	})
+ };
