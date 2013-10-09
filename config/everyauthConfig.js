1f9330ca175650f6894f889893ad479b921e1b4f
macqueen <jessmacqueen@gmail.com>
2013-10-08 17:23:14 -0700

    defined findUserById function so that req.user will work

--- config/everyauthConfig.js
+++ config/everyauthConfig.js
@@ -1,22 +1,22 @@
 var everyauth = require('everyauth');
 var addGitHubUser = require('../app/controllers/users.js').addGitHubUser;
+var findUser = require('../app/controllers/users.js').findUser;
 var https = require('https');
 
 everyauth.debug = true;
 
-// everyauth.everymodule.findUserById(function(userId, cb) {
-//     console.log('findByUserId called');
-//     UserService.findOne({ id: userId }, function(err, user) {
-//         return cb(err, user);
-//     });
-// });
+//everyauth.everymodule.userPkey('github_id');
+
+everyauth.everymodule
+    .findUserById(function (id, callback) {
+      findUser(id, callback);
+    });
 
 everyauth.github
   .scope('user:email')
   .appId(process.env.APP_ID)
   .appSecret(process.env.APP_SECRET)
   .findOrCreateUser(function(session, accessToken, accessTokenExtra, ghUser) {
-    // TODO: write find user function
     console.log('ghUser in findOrCreate everyauth: ', ghUser);
     return false || addGitHubUser(ghUser, accessToken);
   })
