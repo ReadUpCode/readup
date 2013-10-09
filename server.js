9440791583a69d68f1b1116bdd7bdb2364e0fa87
macqueen <jessmacqueen@gmail.com>
2013-10-08 17:17:24 -0700

    configured server.js to use sql session store

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ var express = require('express');
 var everyauth = require('everyauth');
 var everyauthConfig = require('./config/everyauthConfig.js');
 var app = express();
-app.use(express.bodyParser());
+var MySQLSessionStore = require('connect-mysql-session')(express);
 
+app.use(express.bodyParser());
 
 app.configure(function(){
   app.use(express.static(__dirname + "/public"));
   app.use(express.cookieParser());
-  app.use(express.session({secret: process.env.SESSION_SECRET}));
+  app.use(express.session({
+    secret: process.env.SESSION_SECRET,
+    store: new MySQLSessionStore('readup', 'root', '', {})
+  }));
   app.use(everyauth.middleware());
 });
 
