7429a32f919ccbb6449d9ef54f37b6b18169883e
macqueen <jessmacqueen@gmail.com>
2013-10-08 20:19:00 -0700

    refactored tag factory to return a promise to fix async bug

--- public/js/factories/tagsFactory.js
+++ public/js/factories/tagsFactory.js
@@ -3,9 +3,11 @@ var factories = require('../app.js').factories;
 factories.factory('tagsFactory', function($http, $q) {
   var factory = {};
   factory.getAllTags = function(){
-    $http.get('/tags').success(function(res){
-      factory.popularTags = res;
+    var deferred = $q.defer();
+    $http.get('/_/tags').success(function(data){
+      deferred.resolve(data);
     });
+    return deferred.promise;
   };
 
   factory.setTagName = function(tagName) {
@@ -15,7 +17,7 @@ factories.factory('tagsFactory', function($http, $q) {
 
   factory.getTagInfo = function(tagName){
     var deferred = $q.defer();
-    var requestURL = '/tags/' + tagName + '/items';
+    var requestURL = '/_/tags/' + tagName + '/items';
     $http.get(requestURL).success(function(data){
       deferred.resolve(data);
     });
