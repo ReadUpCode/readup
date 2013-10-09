b61d93b9eb52f7f01deaa3dad3f01561f6206fb8
Blake West <bwest87@gmail.com>
2013-10-09 11:30:56 -0700

    latest changes

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -5118,5 +5118,48 @@ factories.factory('tagsFactory', function($http, $q) {
 
   return factory;
 });
-},{"../app.js":1,"./1000SuggestedTags.json":5}]},{},[1,2,3,4,6])
+},{"../app.js":1,"./1000SuggestedTags.json":5}],7:[function(require,module,exports){
+var setupUI = function(){
+
+  // Show and hide input elements (placeholder, suggestions, etc.) at correct times
+  $(".main-input-text").focus(function(){
+    $(this).parent().addClass("typing");
+  }).blur(function(){
+    var thisInput = $(this).parent();
+    setTimeout(function() {
+      thisInput.removeClass("open typing")
+    }, 10);
+  }).keyup(function(){
+    if(!$(this).val()) {
+      $(this).parent().removeClass("has-text");
+    } else {
+      $(this).parent().addClass("has-text");
+    };
+  });
+
+  $("#search-input-tab").click(function(){
+    $("#paste-link-input, #paste-link-input-tab").removeClass("open");
+    $("#search-input, #search-input-tab").toggleClass("open");
+    $("#search-input").find(".main-input-text").focus();
+  });
+
+  $("#paste-link-input-tab").click(function(){
+    $("#search-input, #search-input-tab").removeClass("open");
+    $("#paste-link-input, #paste-link-input-tab").toggleClass("open");
+    $("#paste-link-input").find(".main-input-text").focus();
+  });
+
+  $("#search-input, #search-input-tab, #paste-link-input, #paste-link-input-tab").click(function(event){
+    event.stopPropagation();
+  });
+
+  $('html').click(function() {
+    $("#search-input, #search-input-tab, #paste-link-input, #paste-link-input-tab").removeClass("open typing");
+  });
+};
+
+$(document).on('ready', function() {
+  setupUI();
+});
+},{}]},{},[2,1,3,4,6,7])
 ;
