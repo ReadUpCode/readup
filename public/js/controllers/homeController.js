a15e179bf17de03c47cc1c48ceea2f844be1880b
macqueen <jessmacqueen@gmail.com>
2013-10-08 20:16:55 -0700

    home controller refactor to use promises to get initial tags

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,9 +1,8 @@
 var controllers = require('../app.js').controllers;
 
 controllers.controller('HomeController', ['$scope', '$http', '$location', 'tagsFactory', function($scope, $http, $location, tagsFactory){
-	// this is probably going to cause some async issues
-	tagsFactory.getAllTags();
-  $scope.tags = tagsFactory.popularTags;
+  $scope.tags = tagsFactory.getAllTags();
+  console.log($scope.tags);
   $scope.changeView = function(tagName) {
     tagsFactory.setTagName(tagName);
     $location.path('/' + tagName);
