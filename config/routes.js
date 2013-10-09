e82e372304cda5e8f1fe7e1b08853e7d28a49632
Brett Hoyer <jbh5079@gmail.com>
2013-10-08 19:54:36 -0700

    working on getting image previews of links and storing them to a local file --[WIP]

--- config/routes.js
+++ config/routes.js
@@ -4,18 +4,18 @@ var tags = require('../app/controllers/tags');
 var votes = require('../app/controllers/votes');
 
 module.exports = function(app){
-  app.post('/items', function(req,res){ items.create(req, res); });
-  app.get('/items', function(req,res){ items.get(req, res); });
-  app.get('/items/:id', function(req,res){ items.getOne(req, res); });
-  app.get('/items/:id/score', function(req,res){ items.getScore(req, res); });
+  app.post('/_/items', function(req,res){ items.create(req, res); });
+  app.get('/_/items', function(req,res){ items.get(req, res); });
+  app.get('/_/items/:id', function(req,res){ items.getOne(req, res); });
+  app.get('/_/items/:id/score', function(req,res){ items.getScore(req, res); });
 
-  app.post('/votes', function(req,res){ votes.create(req,res); });
+  app.post('/_/votes', function(req,res){ votes.create(req,res); });
 
-  app.get('/users/:id/items', function(req,res){ users.getAllItemsForUser(req, res); });
+  app.get('/_/users/:id/items', function(req,res){ users.getAllItemsForUser(req, res); });
 
-  app.get('/tags', function(req, res){ tags.get(req, res); });
+  app.get('/_/tags', function(req, res){ tags.get(req, res); });
 
-  app.get('/tags/:tagName/items', function(req, res) { tags.getAllItemsForTag(req, res); });
+  app.get('/_/tags/:tagName/items', function(req, res) { tags.getAllItemsForTag(req, res); });
 
   app.get('/:tagName', function(req, res){
     res.redirect('/#/' + req.params.tagName);
