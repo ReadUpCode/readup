3a12fef19de65ac794082c499a4afe0075a75c8b
Brett Hoyer <jbh5079@gmail.com>
2013-10-09 20:19:30 -0700

    adding directive for display frame

--- public/js/directives/iframeDirective.js
+++
@@ -1,9 +0,0 @@
-var directives = require('../app.js').directives;
-
-directives.directive("showFrame", function(){
- return function(scope, element) {
-      element.bind("mouseenter", function(){
-      element.append('<div>hello</div>');
-      })
-    }
-});
