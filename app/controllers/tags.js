edfdd830443791a9db74743bb81bb0a71eb8e580
Douglas Kong <dougisgreat@gmail.com>
2013-10-10 21:39:12 -0700

    fix server crash when searching for a tag that doesn't exist in the database

--- app/controllers/tags.js
+++ app/controllers/tags.js
@@ -15,36 +15,39 @@ exports.get = function(req, res){
 exports.getAllItemsForTag = function(req, res){
   Tag.find({include: [Item], where: {name: req.params.tagName} }).success(function(tag){
     var responses = [];
-    tag.items.forEach(function(item, index, list){
-      Item.find({include: [Tag], where:{id: item.selectedValues.id}}).success(function(singleItem){
-        Vote.find({where: ['ItemId=? AND UserId=?', item.selectedValues.id, req.user.dataValues.id]})
-        .success(function(userVote){
-          if (userVote){
-            var curUserVote = userVote.selectedValues.value;
-          } else {
-            var curUserVote = 0;
-          }
-          Vote.findAll({ where: {ItemId: item.selectedValues.id }})
-          .success(function(votes){
-            var score=0;
-            for(var j=0; j<votes.length; j++){
-              score+=votes[j].selectedValues.value;
+    
+    if (tag) {
+      tag.items.forEach(function(item, index, list){
+        Item.find({include: [Tag], where:{id: item.selectedValues.id}}).success(function(singleItem){
+          Vote.find({where: ['ItemId=? AND UserId=?', item.selectedValues.id, req.user.dataValues.id]})
+          .success(function(userVote){
+            if (userVote){
+              var curUserVote = userVote.selectedValues.value;
+            } else {
+              var curUserVote = 0;
             }
-            var resItem = {};
-            resItem = singleItem.selectedValues;
-            resItem.curUserVote = curUserVote;
-            resItem.tags = [];
-            resItem.score = score;
-            for (var k=0; k < singleItem.tags.length; k++) {
-              resItem.tags.push(singleItem.tags[k].selectedValues);
-            }
-            responses.push(resItem);
-            if(responses.length === tag.items.length){
-              res.send(responses);
-            }
+            Vote.findAll({ where: {ItemId: item.selectedValues.id }})
+            .success(function(votes){
+              var score=0;
+              for(var j=0; j<votes.length; j++){
+                score+=votes[j].selectedValues.value;
+              }
+              var resItem = {};
+              resItem = singleItem.selectedValues;
+              resItem.curUserVote = curUserVote;
+              resItem.tags = [];
+              resItem.score = score;
+              for (var k=0; k < singleItem.tags.length; k++) {
+                resItem.tags.push(singleItem.tags[k].selectedValues);
+              }
+              responses.push(resItem);
+              if(responses.length === tag.items.length){
+                res.send(responses);
+              }
+            });
           });
         });
       });
-    });
+    }
   });
 };
