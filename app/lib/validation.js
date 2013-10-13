7a6888ba721c50cc29405548c0c31770fa131d1d
Douglas Kong <dougisgreat@gmail.com>
2013-10-13 01:09:10 -0700

    change request validation to use request module instead of phantom because of speed and stability.

--- app/lib/validation.js
+++ app/lib/validation.js
@@ -1,25 +1,21 @@
 var Q = require('q');
-var phantom = require('node-phantom');
+var request = require('request');
 
 var valDeferred;
 
-var validatePhantom = function(link) {
-  phantom.create(function(err,ph) {
-    ph.createPage(function(err,page) {
-      page.open(link, function(err, status) {
-        if (err === null && status==="success") {
-          valDeferred.resolve(true);
-        } else {
-          valDeferred.resolve(false);
-        }
-      });
-    });
+var validateRequest = function(link) {
+  request(link, function(error, response) {
+    if (!error && response.statusCode === 200) {
+      valDeferred.resolve(true);
+    } else {
+      valDeferred.resolve(false);
+    }
   });
 };
 
 exports.validateURL = function(req, res, callback) {
   valDeferred = Q.defer();
-  validatePhantom(req.body.link);
+  validateRequest(req.body.link);
   valDeferred.promise.then(function(validURL){
     if (validURL) {
       callback(req,res);
