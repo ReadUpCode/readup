589108d5a9b2f9eebc4701a346fcab0751d7431e
Brett Hoyer <jbh5079@gmail.com>
2013-10-16 23:15:00 -0700

    changing styling and scrolling of display frame for RAF design functionality

---
+++ public/js/directives/scrollPosition.js
@@ -0,0 +1,42 @@
+var directives = require('../app.js').directives;
+directives.directive('scrollPosition', function($window) {
+
+  return function(scope, element, attrs) {
+    var windowEl = angular.element($window);
+    var previewPane = angular.element('#preview-pane');
+    var origTopDist = +previewPane.css('top').replace('px', '')
+    var setUpScrolling = function(){
+    windowEl.on('scroll', function() {
+      scope.$apply(function() {
+        topDist = previewPane.offset().top
+        scrollDist = windowEl.scrollTop()
+        console.log('origTopDist', origTopDist, 'topDist', topDist, 'scrollDist', scrollDist)
+        if(scrollDist < origTopDist){
+          previewPane.css({top: origTopDist})
+        } else if(scrollDist >= origTopDist){
+          previewPane.css({top: windowEl.scrollTop() + 'px'})
+        }
+      });
+    });
+  }
+
+    var onResize = function(){ 
+      var displayed = previewPane.css('display')
+      previewPane.removeAttr('style');
+      if (displayed !== 'none'){
+        origTopDist = +previewPane.css('top').replace('px', '') 
+        previewPane.show()
+      } else {
+        origTopDist = +previewPane.css('top').replace('px', '') 
+      }
+    }
+
+    var timer;
+    $(window).bind('resize', function(){
+       timer && clearTimeout(timer);
+       timer = setTimeout(onResize, 100);
+    });
+
+    setUpScrolling()
+  }
+});
