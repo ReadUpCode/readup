d2babd2a4d47090745348f0899d5daf549a7bb96
Brett Hoyer <jbh5079@gmail.com>
2013-10-19 12:04:52 -0700

    styling and improving functionality of link form

--- public/js/controllers/formController.js
+++ public/js/controllers/formController.js
@@ -77,6 +77,11 @@ controllers.controller('FormController', ['$scope', '$http', '$modal', '$q', 'ta
         tags: []
       };
       $scope.item.link = '';
+      $scope.item.yourTags = {};
+      $scope.tags = [];
+      for (var each in $scope.types){
+        $scope.types[each].chosen = false;
+      }
     });
   };
 
