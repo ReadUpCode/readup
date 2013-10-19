254a61a94e31205d8d1e4bd011eb1730899a2578
Douglas Kong <dougisgreat@gmail.com>
2013-10-18 21:43:51 -0700

    make hacker news share button render with ng-repeat

---
+++ public/lib/hackerNewsShareButton.js
@@ -0,0 +1,6 @@
+    (function(d, t) {
+        var g = d.createElement(t),
+            s = d.getElementsByTagName(t)[0];
+        g.src = '//hnbutton.appspot.com/static/hn.min.js';
+        s.parentNode.insertBefore(g, s);
+    }(document, 'script'));
