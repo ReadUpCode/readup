f4e29f1654da46a36af3562d664270d4a956f2b7
Douglas Kong <dougisgreat@gmail.com>
2013-10-18 20:20:34 -0700

    make twitter share button directive so that button renders with ng-repeat

---
+++ public/js/directives/twitterShareButton.js
@@ -0,0 +1,21 @@
+var directives = require('../app.js').directives;
+
+directives.directive('twitter', [
+  function() {
+    return {
+      link: function(scope, element, attr) {
+        setTimeout(function() {
+          twttr.widgets.createShareButton(
+            attr.url,
+            element[0],
+            function(el) {}, {
+              count: 'none',
+              text: attr.text,
+              via: attr.via
+            }
+          );
+        });
+      }
+    }
+  }
+]);
