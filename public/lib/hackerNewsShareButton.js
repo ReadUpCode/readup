a243ad59d1a7e42cfc2b023ae402f8cdd3b6b096
Douglas Kong <dougisgreat@gmail.com>
2013-10-19 11:01:14 -0700

    make hacker news button show more consistently using setTimeout

--- public/lib/hackerNewsShareButton.js
+++ public/lib/hackerNewsShareButton.js
@@ -1,6 +1,9 @@
-    (function(d, t) {
+setTimeout(
+	function() {
+		var d = document;
+		var t = 'script';
         var g = d.createElement(t),
             s = d.getElementsByTagName(t)[0];
         g.src = '//hnbutton.appspot.com/static/hn.min.js';
         s.parentNode.insertBefore(g, s);
-    }(document, 'script'));
+}, 150);
