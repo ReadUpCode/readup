49200b0a3bab25726bb846f2344ce4cf0fcdbb4b
stangah <pstanger@gmail.com>
2013-12-12 21:17:35 -0800

    Commented out code used to load old twitter button

--- public/js/directives/twitterShareButton.js
+++ public/js/directives/twitterShareButton.js
@@ -4,16 +4,16 @@ directives.directive('twitter', [
   function() {
     return {
       link: function(scope, element, attr) {
-        setTimeout(function() {
-          twttr.widgets.createShareButton(
-            attr.url,
-            element[0],
-            function(el) {}, {
-              text: attr.text,
-              via: attr.via
-            }
-          );
-        });
+        // setTimeout(function() {
+        //   twttr.widgets.createShareButton(
+        //     attr.url,
+        //     element[0],
+        //     function(el) {}, {
+        //       text: attr.text,
+        //       via: attr.via
+        //     }
+        //   );
+        // });
       }
     }
   }
