178ef27c8f64e0822c534cb5a41d485e5edf91ac
stangah <pstanger@gmail.com>
2013-12-17 13:42:31 -0800

    Reverted encoding argument back to string

--- app/controllers/links.js
+++ app/controllers/links.js
@@ -22,7 +22,7 @@ var topTagArr, topTags;
 // });
 
 // Reads cleaned tags file, and saves to memory;
-topTags = JSON.parse( fs.readFileSync(__dirname + '/../lib/cleanedCuratedTags.json', {encoding: 'utf8'}) );
+topTags = JSON.parse( fs.readFileSync(__dirname + '/../lib/cleanedCuratedTags.json', 'utf8') );
 
 var splitKeywordsRE = /\.| /;
 
